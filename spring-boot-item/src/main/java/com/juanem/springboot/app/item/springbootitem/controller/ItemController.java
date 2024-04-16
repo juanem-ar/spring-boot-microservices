674172ce8ff83647bfb5b5b4039a28ae912beff2
Juan <juanem@hotmail.com>
2024-04-16 20:26:34 -0300

    implementando circuitBreaker en servicio Item

--- spring-boot-item/src/main/java/com/juanem/springboot/app/item/springbootitem/controller/ItemController.java
+++ spring-boot-item/src/main/java/com/juanem/springboot/app/item/springbootitem/controller/ItemController.java
@@ -1,9 +1,13 @@
 package com.juanem.springboot.app.item.springbootitem.controller;
 
 import com.juanem.springboot.app.item.springbootitem.models.dto.Item;
+import com.juanem.springboot.app.item.springbootitem.models.dto.Product;
 import com.juanem.springboot.app.item.springbootitem.service.IItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,14 +17,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemController {
     private final IItemService iItemService;
-
+    private final CircuitBreakerFactory circuitBreakerFactory;
+    @CircuitBreaker(name = "service-items", fallbackMethod = "getItemsFallback")
     @GetMapping("/listar")
     public List<Item> getAllItems(@RequestParam(name="nameParam", required = false) String name, @RequestHeader(name = "token-request", required = false) String token){
         System.out.println("NOMBRE: " + name + ", TOKEN: " + token);
-        return iItemService.getAll();
+        return circuitBreakerFactory.create("items")
+                .run(iItemService::getAll);
     }
     @GetMapping("/{id}/{quantity}")
     public Item getItemById(@PathVariable Long id, @PathVariable Integer quantity){
-        return iItemService.getItemById(id, quantity);
+        return circuitBreakerFactory.create("item")
+                .run(()->iItemService.getItemById(id, quantity));
+    }
+    public ResponseEntity<String> getItemsFallback(Throwable throwable){
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(throwable.getMessage());
     }
 }
