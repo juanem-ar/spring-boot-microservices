4d3de886bff7cd21e5023706d73f3a4b5d85d9c0
Juan <juanem@hotmail.com>
2024-04-15 21:03:58 -0300

    agregando pre y post filter al api gateway

---
+++ api-gateway/src/main/java/com/juanem/springboot/app/api_gateway/filters/GlobalFilters.java
@@ -0,0 +1,40 @@
+package com.juanem.springboot.app.api_gateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import org.springframework.http.MediaType;
+
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class GlobalFilters implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Ejecutando pre filtro");
+        exchange.getRequest().mutate().headers(h -> {
+            h.add("token", "12345");
+        });
+        //TODO PRE FILTER
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            log.info("Ejecutando post filtro");
+            //TODO POST FILTER
+            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+                exchange.getResponse().getHeaders().add("token", valor);
+            });
+            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+            exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
--- out/production/spring-boot-microservices/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO products (name, price, creation_date) VALUES ('Panasonic', 800, NOW());
-INSERT INTO products (name, price, creation_date) VALUES ('Sony', 800, NOW());
-INSERT INTO products (name, price, creation_date) VALUES ('HP', 800, NOW());
-INSERT INTO products (name, price, creation_date) VALUES ('Nike', 800, NOW());
